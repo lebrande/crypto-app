6792f41ccc34d9f538f4f598c416e66b62df2f02
Kuba <jakub.pusiak@gmail.com>
2022-04-19 09:48:03 +0200

    button connect Metamask

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,30 @@
+import { useConnection } from "../src/connection/context";
+
 export const HomePage = () => {
+  const { connectionState, connect } = useConnection();
+
+  const isError = connectionState.type === 'METAMASK_ERROR';
+  const isConnected = connectionState.type === 'METAMASK_CONNECTED';
+
+  if (isError) {
+    return (
+      <h2>Error: {connectionState.error.message}</h2>
+    );
+  }
+
+  if (isConnected) {
+    return (
+      <>
+        <h2>Your Matamask wallet is connected!</h2>
+        <p>Your wallet address: {connectionState.connection.myWalletAddress}</p>
+      </>
+    );
+  }
+
   return (
-    <div>HomePage</div>
+    <button type="button" onClick={connect}>
+      Connect Metamask
+    </button>
   );
 };
 
